93aae669d6f8680d4d3bf2af85407d2dbb9647d4
Raghvendra Singh <2023bcs065@axiscolleges.in>
2026-02-12 22:42:41 +0530

    Change base path in Vite config for deployment
    Updated base path for deployment to GitHub Pages.

--- Studex/vite.config.ts
+++ Studex/vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Studex/", // Add this line for GitHub Pages
-  optimizeDeps: {
-    exclude: ["lucide-react"],
-  },
+  base: "/AriseYUKTI/",
 });
